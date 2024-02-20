17e3e06cf6080bd05ebd41536329fd0f8ef344e1
Ifeoluwa <dloveade@mail.com>
2024-02-21 00:53:41 +0100

    Set automountServiceAccountToken to false

--- todo-app-backend/app.py
+++ todo-app-backend/app.py
@@ -12,7 +12,6 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # Access the database URL from the environment
 DATABASE_URL = os.getenv("DATABASE_URL")
-# app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASSWORD"]}@{os.environ["POSTGRES_HOST"]}:{os.environ["POSTGRES_PORT"]}/{os.environ["POSTGRES_DB"]}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
