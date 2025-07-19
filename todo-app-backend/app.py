972a04a7f53819be84dabbdcfbe71d21ae70d8c6
danade002 <danielademeso1@gmail.com>
2025-07-19 10:01:16 +0100

    Dockerizing the frontend and the backend of the application

--- todo-app-backend/app.py
+++ todo-app-backend/app.py
@@ -12,7 +12,8 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # Access the database URL from the environment
 DATABASE_URL = os.getenv("DATABASE_URL")
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASSWORD"]}@{os.environ["POSTGRES_HOST"]}:{os.environ["POSTGRES_PORT"]}/{os.environ["POSTGRES_DB"]}'
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASSWORD"]}@{os.environ["POSTGRES_HOST"]}:{os.environ["POSTGRES_PORT"]}/{os.environ["POSTGRES_DB"]}'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@db:5432/mydb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
